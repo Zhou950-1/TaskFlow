751b1efc664de60b92e37fe222c56dcb8ec5d84d
zhou zhihui <zhihui@debian-hp.debian13.local>
2025-09-25 09:50:46 +0200

    First Commit: proyect structure and README

---
+++ README.md
@@ -0,0 +1,2 @@
+**Nombre:** Zhou Zhihui
+**Comentario:** Un proyecto de prueba llamado TaskFlow, desplegado con XAMPP en Debian 13 situado en el directorio raíz /opt/lampp/htdocs/TaskFlow
---
+++ public/public.php
@@ -0,0 +1,6 @@
+<?php
+// Mensaje en el navegador
+echo "<h1>¡Hola! Todo esto funciona 🚀</h1>";
+
+// Información del servidor (opcional)
+echo "<p>Estás ejecutando PHP versión: " . phpversion() . "</p>";
