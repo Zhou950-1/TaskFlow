b2d1a3c101f9541b3d54a484cc6ffb35fca7bb97
zhou zhihui <zhihui@debian-hp.debian13.local>
2025-11-26 19:34:13 +0100

    __DIR__ agregado en AuthTest.php

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -3,9 +3,11 @@
 use PHPUnit\Framework\TestCase;
 
 // Incluimos los ficheros que contienen la lógica que queremos probar
-// Ruta adaptada al contenedor docker apache-php con rutas absolutas dentro del contenedor
-require_once '/var/www/TaskFlow/app/data.php';
-require_once '/var/www/TaskFlow/app/controllers/AuthController.php';
+// Ruta adaptada al contenedor docker apache-php con rutas absolutas dentro del contenedor (modificado por __DIR__)
+// __DIR__ concatena la ruta para incluir la /
+// De esta forma __DIR__ apunta siempre al directorio actual del fichero tests y sube un nivel hasta TaskFlow
+require_once __DIR__ . '/../app/data.php';
+require_once __DIR__ . '/../app/controllers/AuthController.php';
 
 class AuthTest extends TestCase {
     // Prueba para el "camino feliz" (login correcto)
@@ -13,8 +15,7 @@ class AuthTest extends TestCase {
 
         global $usuarios_bbdd; // Accedemos a los usuarios de prueba
         // Actuar: llamamos a la función
-        $resultado = handleLogin('usuario1@taskflow.com', 'pass123',
-        $usuarios_bbdd);
+        $resultado = handleLogin('usuario1@taskflow.com', 'pass123', $usuarios_bbdd);
         // Aserción: Afirmamos que el resultado debe ser verdadero
 
         $this->assertTrue($resultado);
@@ -26,8 +27,7 @@ class AuthTest extends TestCase {
         global $usuarios_bbdd;
         
         // Actuar: llamamos con una contraseña errónea
-        $resultado = handleLogin('usuario1@taskflow.com', 'pass_erroneo',
-        $usuarios_bbdd);
+        $resultado = handleLogin('usuario1@taskflow.com', 'pass_erroneo', $usuarios_bbdd);
 
         // Aserción: Afirmamos que el resultado debe ser falso
         $this->assertFalse($resultado);
